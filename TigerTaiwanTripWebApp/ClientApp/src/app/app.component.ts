c554342280349d02831bb0847c1da7133b9092a8
Kwei-Gang Lai <lai225joseph@hotmail.com>
2019-07-22 16:06:45 +0800

    fix nav menu

--- TigerTaiwanTripWebApp/ClientApp/src/app/app.component.ts
+++ TigerTaiwanTripWebApp/ClientApp/src/app/app.component.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  loginUser: string = localStorage.getItem('login');
+export class AppComponent implements AfterViewInit {
+  loginUser: string = "";
   title = 'app';
 
   constructor() {
+    console.log(localStorage.getItem("login"));
     if (localStorage.getItem("login") === null) {
       localStorage.setItem('login', '');
     } 
@@ -18,5 +19,9 @@ export class AppComponent {
   logout() {
     localStorage.setItem('login', '');
     location.href = './trip-type';
+  }
+
+  ngAfterViewInit() {
+    this.loginUser = localStorage.getItem('login');
   }
 }
